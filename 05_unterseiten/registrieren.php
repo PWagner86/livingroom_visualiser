140c06b33c41d8246cf6505edd23fd0f3b8c4568
Peter Wagner <peter.wagner1986@gmail.com>
2020-12-06 14:39:08 +0100

    added error messages if input field is empty. no sanatizing yet

--- 05_unterseiten/registrieren.php
+++ 05_unterseiten/registrieren.php
@@ -10,7 +10,18 @@ if( isset($_POST['vorname']) &&
     isset($_POST['plz']) &&
     isset($_POST['ort']) &&
     isset($_POST['email']) &&
-    isset($_POST['passwort'])
+    isset($_POST['passwort'])&&
+    isset($_POST['passwort2'])&&
+    isset($_POST['agb'])&&
+    !empty($_POST['vorname'])&&
+    !empty($_POST['nachname'])&&
+    !empty($_POST['adresse'])&&
+    !empty($_POST['plz'])&&
+    !empty($_POST['ort'])&&
+    !empty($_POST['email'])&&
+    !empty($_POST['passwort'])&&
+    !empty($_POST['passwort2'])&&
+    !empty($_POST['agb'])
 ){
     $vorname = $_POST['vorname'];
     $nachname = $_POST['nachname'];
@@ -29,6 +40,8 @@ if( isset($_POST['vorname']) &&
     // header('Location: login.php');
 
     echo 'Sie sind registriert und können sich nun einloggen.';
+}else{
+    echo 'Etwas ist schiefgelaufen';
 }
 
 ?>
@@ -56,9 +69,11 @@ if( isset($_POST['vorname']) &&
                 <h3>Registrieren</h3>
             </div>
             <div class="first-name-wrapper reg-wrapper" id="mobile-first-name">
+            <?php if(empty($_POST['vorname'])){?>
                 <div class="error">
-
+                <?php echo '<p>Bitte Vornamen eingeben</p>'?>
                 </div>
+            <?php }?>
                 <div class="first-name-input-wrapper input-wrapper">
                     <label for="vorname">Vorname :</label>
                     <input type="text" name="vorname">
@@ -66,9 +81,11 @@ if( isset($_POST['vorname']) &&
             </div>
             </div>
             <div class="last-name-wrapper reg-wrapper" id="mobile-last-name">
+            <?php if(empty($_POST['nachname'])){?>
                 <div class="error">
-
+                <?php echo '<p>Bitte Nachname eingeben</p>'?>
                 </div>
+            <?php }?>    
                 <div class="last-name-input-wrapper input-wrapper">
                     <label for="nachname">Nachname :</label>
                     <input type="text" name="nachname">
@@ -77,9 +94,11 @@ if( isset($_POST['vorname']) &&
             <div class="placeholder-wrapper reg-wrapper" id="mobile-placeholder"></div>
             </div>
             <div class="adresse-wrapper reg-wrapper" id="mobile-adresse">
+            <?php if(empty($_POST['adresse'])){?>
                 <div class="error">
-
+                <?php echo '<p>Bitte Adresse eingeben</p>'?>
                 </div>
+            <?php }?>    
                 <div class="adresse-input-wrapper input-wrapper">
                     <label for="adresse">Adresse :</label>
                     <input type="text" name="adresse">
@@ -87,9 +106,11 @@ if( isset($_POST['vorname']) &&
             </div>
             </div>
             <div class="plz-wrapper reg-wrapper" id="mobile-plz">
+            <?php if(empty($_POST['plz'])){?>
                 <div class="error">
-
+                <?php echo '<p>Bitte Postleitzahl eingeben</p>'?>
                 </div>
+            <?php }?>    
                 <div class="plz-name-input-wrapper input-wrapper">
                     <label for="plz">PLZ :</label>
                     <input type="number" name="plz">
@@ -97,9 +118,11 @@ if( isset($_POST['vorname']) &&
             </div>
             </div>
             <div class="city-wrapper reg-wrapper" id="mobile-city">
+            <?php if(empty($_POST['ort'])){?>
                 <div class="error">
-
+                <?php echo '<p>Bitte Ort eingeben</p>'?>
                 </div>
+            <?php }?>
                 <div class="city-input-wrapper input-wrapper">
                     <label for="ort">Ort :</label>
                     <input type="text" name="ort">
@@ -107,9 +130,11 @@ if( isset($_POST['vorname']) &&
             </div>
             </div>
             <div class="email-wrapper reg-wrapper" id="mobile-email">
+            <?php if(empty($_POST['email'])){?>
                 <div class="error">
-              
+                <?php echo '<p>Bitte gültige E-Mail eingeben</p>'?>
                 </div>
+            <?php }?>
                 <div class="email-input-wrapper input-wrapper">
                     <label for="email">E-Mail :</label>
                     <input type="email" name="email">
@@ -117,27 +142,33 @@ if( isset($_POST['vorname']) &&
             </div>
             </div>
             <div class="password-wrapper reg-wrapper" id="mobile-password">
+            <?php if(empty($_POST['passwort'])){?>
                 <div class="error">
-
+                <?php echo '<p>Bitte Passwort eingeben</p>'?>
                 </div>
+            <?php }?>
                 <div class="password-input-wrapper input-wrapper">
                     <label for="passwort">Passwort :</label>
                     <input type="password" name="passwort">
                 </div>
             </div>
             <div class="second-password-wrapper reg-wrapper" id="mobile-second-password">
+            <?php if(empty($_POST['passwort2'])){?>
                 <div class="error">
-
+                <?php echo '<p>Bitte Passwort nochmals eingeben</p>'?>
                 </div>
+            <?php }?>
                 <div class="second-password-input-wrapper input-wrapper">
                     <label for="passwort2">Passwort <br> wiederholen :</label>
                     <input type="password" name="passwort2">
                 </div>
             </div>
             <div class="agb-wrapper reg-wrapper" id="mobile-agb">
+            <?php if(empty($_POST['agb'])){?>
                 <div class="error">
-
+                <?php echo '<p>Bitte AGBs bestätigen</p>'?>
                 </div>
+            <?php }?>
                 <div class="agb-input-wrapper input-wrapper">
                     <label for="agb">Ich habe die AGB gelesen :</label>
                     <input type="checkbox" name="agb">
